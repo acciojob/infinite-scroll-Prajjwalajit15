1f12db7f8e2e5bad5eea1880af4a64702e337cf4
prajjwal ajit <122018880+Prajjwalajit15@users.noreply.github.com>
2023-11-25 17:31:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
 //your code here!
+const sc = document.getElementById("infi-list");
+let ct = 1;
+for(;ct<=10; ct++){
+	const li = document.createElement("li");
+	li.innerText = 'item ' +ct;
+	li.className = "item";
+	sc.appendChild(li);
+}
 
+sc.addEventListener("scroll",()=>{
+	let sh = sc.scrollTop;
+	let mxSH= sc.scrollHeight-sc.clientHeight;
+
+	if (mxSH-sh<=5) {
+		aTMI();
+	}
+})
+function aTMI() {
+	const li1 = document.createElement("li");
+	li1.innerText = 'item ' +ct++;
+	li1.className = "item";
+	const li2 = document.createElement("li");
+	li2.innerText = 'item ' +ct++;
+	li2.className = "item";
+	sc.append(li1,li2)
+}
